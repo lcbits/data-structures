cb34209c22e379919165c7c4f8899bf940097525
lcbits <lynnc24@gmail.com>
2016-04-21 22:06:37 -0700

    finished linked list and have a buggy version of tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.tail === null){
+      list.tail = Node(value); //1. set tail to the value being passed in
+      list.head = list.tail; //2. set the head and tail to the same value
+      list.head.next = Node(value);
+    } else {
+       // list[list.tail.value] = list.tail; 
+        list.tail.next = Node(value); //setting old list.tail's pointer to the new tail node
+        list[list.tail.value] = list.tail; //making a new list node for the former ta
+        list.tail = Node(value); //1. set tail to the new node
+
+         
+    }
+    list[value] = Node(value);
   };
 
   list.removeHead = function() {
+
+    var temp = list.head.next;  
+    var headValue = list.head.value;  //1. set temp to hold current head value
+     delete list[list.head.value];
+     delete list.head; // 2. delete head value
+    list.head = temp;  // 3. reset head value to the next value
+    return headValue; // 4. return 
+
   };
 
   list.contains = function(target) {
+    //loop through list using for in loop
+    return list.hasOwnProperty(target) ? true : false;
   };
 
   return list;
@@ -23,6 +46,17 @@ var Node = function(value) {
 
   return node;
 };
+
+
+// var checkList = LinkedList();
+// checkList.addToTail(4);
+// checkList.addToTail(5);
+// checkList.addToTail(6);
+// checkList.addToTail(7)
+// checkList.removeHead()
+// console.log(checkList);
+// console.log(checkList.contains(5));
+// console.log(checkList.contains(7));
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  //this array is made up of newTree "nodes"
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,59 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+
+  var recurseFunc = function (node, target){
+
+    //base case: if all nodes have been searched, array is empty 
+        //return false
+
+    //if the node's value contains target, return true
+    if(node.value === target){
+      return true;
+    }
+
+    //recursive case: if the node's children array has nodes, loop through each node
+    if(node.children.length > 0){
+      for(var i=0; i<node.children.length; i++){
+        recurseFunc(node.children[i], target);
+      }
+      return false;
+    }
+
+  };
+
+  return recurseFunc(this,target);
+
+
+
+
+  // var recurseFunc = function(childrenObj, target){
+   
+  //  if(childrenObj.value === target){
+  //   return true;
+  //  }
+
+  //  for(var i=0; i<childrenObj.children.length; i++){
+  //   return  recurseFunc(childrenObj.children[i].children, target);
+  //  }
+
+  // };
+    
+  // recurseFunc(this.children, target);
+
+
+  // return _.reduce(this.children, function(passedTest, curr){ //curr = this.children[i] which is an object
+  //   if(curr.value === target){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }, false);
+
 };
 
 
@@ -21,3 +73,12 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // var testTree = Tree();
+ // testTree.addChild(5);
+ // testTree.addChild(6);
+ // testTree.addChild(7);
+ // testTree.children[0].addChild(7);
+ // testTree.children[1].addChild(9);
+ // console.log('this.children', this.children);
+ // console.log(testTree.contains(6));
+ // console.log(testTree);
